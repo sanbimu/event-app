305e546d2268966262a0daeec49bb6dd393c443f
Nathalie Appaerts <119463187+sanbimu@users.noreply.github.com>
2023-04-26 22:35:23 +0200

    Tailwind config

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -3,8 +3,33 @@ module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      colors: {},
-      fontFamily: {},
+      fontSize: {
+        xxs: '10px',
+      },
+      boxShadow: {
+        custom: '4px 4px 0px rgb(213, 193, 174)',
+      },
+      colors: {
+        background: '#F3ECE8',
+        black: '#1E1E1E',
+        'dark-grey': '#323030',
+        beige: '#D5C1AE',
+        'beige-muted': '#DFD1C3',
+        aubergine: '#6F4B4F',
+        pink: '#D48F84',
+        'dark-pink': '#B67668',
+        'dark-pink-muted': '#BE857A',
+        'dark-pink-transparent': '#E4BDB6',
+        orange: '#CB730D',
+        'orange-muted': '#D99C59',
+        brown: '#6A563F',
+        'brown-muted': '#8B7C69',
+      },
+      fontFamily: {
+        franklin: ['libre-franklin', 'sans-serif'],
+        fields: ['fields-display', 'sans-serif'],
+        fromage: ['fromage', 'sans-serif'],
+      },
       screens: {},
     },
   },
