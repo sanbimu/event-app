0fe2102e25d1a48a00e05c74d18a619892715ff8
Nathalie Appaerts <119463187+sanbimu@users.noreply.github.com>
2023-05-05 10:59:07 +0200

    ShowOne

--- client/src/components/NewEvent.tsx
+++ client/src/components/NewEvent.tsx
@@ -2,16 +2,13 @@ import React from 'react';
 
 interface NewEventProps {
   className?: string;
-  onClick?: React.MouseEventHandler<HTMLDivElement>;
+
   children: React.ReactNode;
 }
 
-const NewEvent: React.FC<NewEventProps> = ({ className, onClick, children }) => {
+const NewEvent: React.FC<NewEventProps> = ({ className, children }) => {
   return (
-    <div
-      className="flex w-full flex-row border border-black shadow-custom"
-      onClick={onClick}
-    >
+    <div className="flex w-full flex-row border border-black shadow-custom">
       {children}
     </div>
   );
--- client/src/layout/ShowAll.tsx
+++ client/src/layout/ShowAll.tsx
@@ -2,8 +2,16 @@ import React from 'react';
 import Button from '../components/Button';
 import EventCard from '../components/EventCard';
 import { events } from '../shared/constants';
+import ShowOne from '../components/windows/ShowOne';
+import { useWindowContext } from '../hooks';
 
 const ShowAll: React.FC = () => {
+  const { openWindow } = useWindowContext();
+
+  const handleShowOne = () => {
+    openWindow({ content: <ShowOne /> });
+  };
+
   return (
     <>
       <div className="flex flex-col">
@@ -11,9 +19,11 @@ const ShowAll: React.FC = () => {
           {events.map((event, index) => {
             const position = index % 2 === 0 ? 1 : 2;
             const height = Math.floor(Math.random() * (200 - 150) + 150);
-
             return (
-              <div className="border-overlap-br flex break-inside-avoid items-center justify-center border border-black p-4">
+              <div
+                className="border-overlap-br flex cursor-pointer break-inside-avoid items-center justify-center border border-black p-4"
+                onClick={handleShowOne}
+              >
                 <EventCard
                   height={height}
                   position={position}
